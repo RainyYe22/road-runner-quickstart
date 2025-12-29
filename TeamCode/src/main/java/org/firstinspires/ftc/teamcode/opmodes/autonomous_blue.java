1cec703336584b4991ed92658d9affe964ae81b7
Camelia222 <rainyye2@gmail.com>
2025-12-29 10:18:37 -0500

    h

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous_blue.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.field_constants;
+import org.firstinspires.ftc.teamcode.subsystems.vision.vision_imu;
+
+@Autonomous(name = "autonomous_blue")
+public class autonomous_blue extends LinearOpMode {
+
+    private MecanumDrive drive;
+    private vision_imu turret;
+    private DcMotor intakeOne, intakeTwo;
+    private DcMotor launcher;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new MecanumDrive(hardwareMap, field_constants.blue_left);  // start pose
+        turret = new vision_imu(hardwareMap);
+
+        intakeOne = hardwareMap.get(DcMotor.class, "intakeOneMotor");
+        intakeTwo = hardwareMap.get(DcMotor.class, "intakeTwoMotor");
+
+        launcher = hardwareMap.get(DcMotor.class, "launcherMotor");
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.actionBuilder(field_constants.blue_left)
+                .lineToX(field_constants.blue_intake.position.x)
+                .lineToY(field_constants.blue_intake.position.y)
+                .build();
+
+        intakeOne.setPower(1);
+        intakeTwo.setPower(1);
+        sleep(2000);
+        intakeOne.setPower(0);
+        intakeTwo.setPower(0);
+
+
+        drive.actionBuilder(field_constants.blue_intake)
+                .lineToX(field_constants.BLUE_GOAL_CENTER.x)
+                .lineToY(field_constants.BLUE_GOAL_CENTER.y)
+                .build();
+
+        turret.setTargetHeading(Math.atan2(
+                field_constants.BLUE_GOAL_CENTER.y - field_constants.blue_intake.position.y,
+                field_constants.BLUE_GOAL_CENTER.x - field_constants.blue_intake.position.x
+        ));
+        sleep(500); 
+
+
+        launcher.setPower(1);
+        sleep(1000);
+        launcher.setPower(0);
+
+        telemetry.addLine("Autonomous complete!");
+        telemetry.update();
+    }
+}
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/field_constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/field_constants.java
@@ -1,22 +1,39 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 
-public class field_constants{
-    public static final Pose2d blue_left = new Pose2d(0,0,0);
-    public static final Pose2d blue_right = new Pose2d(0,0,0);
-    public static final Pose2d red_left = new Pose2d(0,0,0);
-    public static final Pose2d red_right = new Pose2d(0,0,0);
+public class field_constants {
 
-    public static final Pose2d red_goal = new Pose2d(0,0,0);
-    public static final Pose2d blue_goal = new Pose2d(0,0,0);
+    public static final Pose2d blue_left  = new Pose2d(0, 0, 0);
+    public static final Pose2d blue_right = new Pose2d(0, 0, 0);
+    public static final Pose2d red_left   = new Pose2d(0, 0, 0);
+    public static final Pose2d red_right  = new Pose2d(0, 0, 0);
 
-    public static final Pose2d red_end = new Pose2d(0,0,0);
-    public static final Pose2d blue_end = new Pose2d(0,0,0);
+    public static final Pose2d red_goal  = new Pose2d(0, 0, 0);
+    public static final Pose2d blue_goal = new Pose2d(0, 0, 0);
+
+    public static final Vector2d RED_GOAL_CENTER =
+            new Vector2d(red_goal.position.x, red_goal.position.y);
+    public static final Vector2d BLUE_GOAL_CENTER =
+            new Vector2d(blue_goal.position.x, blue_goal.position.y);
+
+    public static final Pose2d red_end  = new Pose2d(0, 0, 0);
+    public static final Pose2d blue_end = new Pose2d(0, 0, 0);
+
+    public static final Pose2d blue_intake = new Pose2d(24, 48, 0); // example: x=24", y=48"
+    public static final Pose2d red_intake  = new Pose2d(120, 48, Math.PI); // mirrored for red
 
     public static final double robot_dimensions = 7.0;
+    public static final double LAUNCH_RADIUS = 24.0; // inches (TUNE THIS)
+    public static final double LAUNCH_RADIUS_WITH_BUFFER =
+            LAUNCH_RADIUS - robot_dimensions;
 
+    public static final double FIELD_SIZE = 144.0; // inches
 }
+
+
+
 
 
 
